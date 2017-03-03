6ced14019789bfbeb858c92a04558813ab8724d6
socratess <spalaciosm@unal.edu.co>
2017-03-03 00:59:56 -0500

    huyhh

---
+++ src/main/hellotu/logeo.java
@@ -0,0 +1,16 @@
+<!DOCTYPE>
+<html>
+<head>
+   <title>SUB-SH</title>
+</head>
+  <h1><center>SUB-SH</center></h1>
+<body>
+<form method="POST">
+    usuario o correo de logeo:<input type="text" name="ts"><br/><br/>
+    contraseña:<input type="text" name="ct"><br/><br/>
+
+     <input type="submit" value="Iniciar" name="botonI"><br/><br/>
+</form>
+
+</body>
+</html>
